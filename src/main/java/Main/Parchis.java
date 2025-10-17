2eb2f2d0588ee2c669c09059a8bf26085ec89612
AbigailAle95 <Abigailalech@gmail.com>
2025-10-17 15:12:47 -0600

    Primer commit

---
+++ src/main/java/Main/Parchis.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package Main;
+
+import View.GUIInicio;
+
+/**
+ *
+ * @author abiga
+ */
+public class Parchis {
+
+    public static void main(String[] args) {
+        GUIInicio v1 = new GUIInicio ();
+        v1.setVisible(true);
+        v1.setLocationRelativeTo(null);
+    }
+}
